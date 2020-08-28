edc7ae5566c070c7a601b9b61eddf174a48ec8a4
Isaiah <greatpup1940@gmail.com>
2020-08-28 10:39:59 -0400

    Finished form validation

--- src/components/register/register.jsx
+++ src/components/register/register.jsx
@@ -4,10 +4,17 @@ import { register } from '../../redux/actions';
 import { useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom'
 import './register.styles.scss'
+import * as Yup from 'yup';
 
 const Register = () => {
     const dispatch = useDispatch();
     const history = useHistory()
+    const [errors, setErrors] = useState({
+        name: '',
+        email: "",
+        password: '',
+        confirmPassword: ''
+    })
 
     const [formValues, setFormValues] = useState({
         name: '',
@@ -16,18 +23,50 @@ const Register = () => {
         confirmPassword: ''
     });
 
+
+    const formSchema = Yup.object().shape({
+        name: Yup.string().required('Please enter your name'),
+        email: Yup.string().email('Must be a valid email address.').required('Must include email address.'),
+        password: Yup.string().min(6, 'Password must be at least 6 characters long.').required('Password is Required'),
+        confirmPassword: Yup.mixed()
+            .oneOf([formValues.password], 'passwords must match')
+            .required('Please confirm your password')
+    });
+
+
     const handleChange = (e) => {
-        const { name, value } = e.target;
+        e.persist();
+        Yup.reach(formSchema, e.target.name)
+            .validate(e.target.value)
+            .then((valid) => {
+                setErrors({
+                    ...errors,
+                    [e.target.name]: ''
+                });
+            })
+            .catch((err) => {
+                setErrors({
+                    ...errors,
+                    [e.target.name]: err.message
+                });
+            });
+
         setFormValues({
             ...formValues,
-            [name]: value
+            [e.target.name]: e.target.value
         });
     };
 
     const registerUser = (e) => {
         e.preventDefault();
-        
-        dispatch(register(formValues, history))
+        formSchema.isValid(formValues).then((valid) => {
+            if (valid) {
+                dispatch(register(formValues, history))
+            } else if (!valid) {
+                // setting global error
+                dispatch({ type: 'ERROR', payload: { error: 'Please Fill Out All Fields' } });
+            }
+        });
     };
 
     const clearForm = () => {
@@ -39,44 +78,100 @@ const Register = () => {
     return (
         <form className="form-container" onSubmit={registerUser}>
             <div className="text-field-register-container">
+
+
                 <TextField
                     value={formValues.name}
                     onChange={handleChange}
-                    color="secondary"
                     type="text"
-                    label="Name"
+                    label="name"
                     name="name"
                     required
                 />
-                <TextField
-                    value={formValues.email}
-                    onChange={handleChange}
-                    color="secondary"
-                    type="text"
-                    label="Email"
-                    name="email"
-                    required
-                />
-                <TextField
-                    value={formValues.password}
-                    onChange={handleChange}
-                    color="secondary"
-                    type="password"
-                    label="Password"
-                    name="password"
-                    required
-                />
-                <TextField value={formValues.confirmPassword}
-                    onChange={handleChange}
-                    color="secondary"
-                    type="password"
-                    label="confirm password"
-                    name="confirmPassword"
-                    required>
-                </TextField>
-                <Button type="submit" color="secondary" variant="outlined" id='submit-btn'>
-                    Register
-				</Button>
+
+
+                {errors.email.length ?
+                    <TextField
+                        value={formValues.email}
+                        onChange={handleChange}
+                        type="text"
+                        label="Email"
+                        name="email"
+                        required
+                        error
+                        helperText={errors.email}
+                    />
+                    :
+                    <TextField
+                        value={formValues.email}
+                        onChange={handleChange}
+                        type="text"
+                        label="Email"
+                        name="email"
+                        required
+                    />}
+
+                {errors.password.length ?
+                    <TextField
+                        value={formValues.password}
+                        onChange={handleChange}
+                        type="password"
+                        label="Password"
+                        name="password"
+                        required
+                        error
+                        helperText={errors.password}
+                    /> :
+                    <TextField
+                        value={formValues.password}
+                        onChange={handleChange}
+                        type="password"
+                        label="Password"
+                        name="password"
+                        required
+                    />
+                }
+
+                {errors.confirmPassword.length ?
+                    <TextField value={formValues.confirmPassword}
+                        onChange={handleChange}
+                        type="password"
+                        label="confirm password"
+                        name="confirmPassword"
+                        required
+                        error
+                        helperText={errors.confirmPassword}
+                    >
+
+                    </TextField>
+
+                    :
+
+                    <TextField value={formValues.confirmPassword}
+                        onChange={handleChange}
+                        type="password"
+                        label="confirm password"
+                        name="confirmPassword"
+                        required>
+                    </TextField>
+
+                }
+
+                {errors.password.length || errors.confirmPassword.length || errors.email.length
+                    ?
+                    <Button disabled type="submit" color="secondary" variant="outlined" id='submit-btn'>
+                        Register
+                    </Button>
+                    :
+                    <Button type="submit" color="secondary" variant="outlined" id='submit-btn'>
+                        Register
+				    </Button>
+                }
+
+
+
+
+
             </div>
         </form>
     );
